074b46d3e5be7e646030eacc0307103a3b1585cd
Masato Hori <masato@masato-946gz.(none)>
2012-12-10 23:41:09 +0900

    initial commit

---
+++ lib/src/AnimalSummary.dart
@@ -0,0 +1,10 @@
+library dart_web_ui_sample;
+import 'package:dart_web_ui_sample/src/Color.dart';
+
+class AnimalSummary {
+    AnimalSummary(this.name, this.count, this.color);
+    String name = "No Name";
+    int count = 0;
+    Color color;
+}
+
---
+++ lib/src/Color.dart
@@ -0,0 +1,10 @@
+library dart_web_ui_sample;
+
+class Color {
+    Color(this.red, this.green, this.blue);
+    int red;
+    int green;
+    int blue;
+    String get cssString => "rgb(${red},${green},${blue})";
+}
+
